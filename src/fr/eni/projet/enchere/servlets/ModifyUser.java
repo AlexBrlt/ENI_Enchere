716b1c79cf3704b2553638ee920273225a07707f
GLauProvisoire <91891179+GLauProvisoire@users.noreply.github.com>
2021-12-08 15:07:01 +0100

    Create ModifyUser.java

---
+++ src/fr/eni/projet/enchere/servlets/ModifyUser.java
@@ -0,0 +1,32 @@
+package fr.eni.projet.enchere.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ModifyUser
+ */
+@WebServlet(name = "ModifyUserServlet", urlPatterns = { "/ModifyUser" })
+public class ModifyUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModifyUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/JSP/ModifyUser.jsp").forward(request, response);
+	}
+}
