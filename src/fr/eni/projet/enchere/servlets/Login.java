b1e3290e85ce2b0a3da622a0a7b0ae3965893412
lderville <91890830+lderville@users.noreply.github.com>
2021-12-08 17:28:06 +0100

    Update Login.java

--- src/fr/eni/projet/enchere/servlets/Login.java
+++ src/fr/eni/projet/enchere/servlets/Login.java
@@ -56,22 +56,36 @@ public class Login extends HttpServlet {
 			e.printStackTrace();
 		}
 		
-		User user = utilisateurs.get(0);
-	
-		String pseudo = user.getPseudo();
-		String mot_de_passe = user.getMot_de_passe();
-	
 		
 		
-			if (pseudo.equals(userID) && mot_de_passe.equals(password)) {
-				
-				RequestDispatcher dispatcher = request.getRequestDispatcher(VUETRUE);
-				dispatcher.forward(request, response);
-				
-			} else {
-				System.out.println("Coucou");
-				RequestDispatcher dispatcher = request.getRequestDispatcher(VUEFALSE);
-				dispatcher.forward(request, response);	
-			}	
+		if (utilisateurs.isEmpty()) {
+			
+			System.out.println("Coucou");
+			RequestDispatcher dispatcher = request.getRequestDispatcher(VUEFALSE);
+			dispatcher.forward(request, response);
+			
+			
+		} else {
+			
+			User user = utilisateurs.get(0);
+			String pseudo = user.getPseudo();
+			String mot_de_passe = user.getMot_de_passe();
+		
+			
+			
+				if (pseudo.equals(userID) && mot_de_passe.equals(password)) {
+					
+					RequestDispatcher dispatcher = request.getRequestDispatcher(VUETRUE);
+					dispatcher.forward(request, response);
+					
+				} else {
+					System.out.println("Coucou");
+					RequestDispatcher dispatcher = request.getRequestDispatcher(VUEFALSE);
+					dispatcher.forward(request, response);	
+				}
+
+		}
+		
+	
 	}
 }
