2257428a37873b1088eb623ae5449ad403039515
GLauProvisoire <91891179+GLauProvisoire@users.noreply.github.com>
2021-12-09 17:54:03 +0100

    Update ModifyArticle.java

--- src/fr/eni/projet/enchere/servlets/ModifyArticle.java
+++ src/fr/eni/projet/enchere/servlets/ModifyArticle.java
@@ -1,11 +1,17 @@
 package fr.eni.projet.enchere.servlets;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.projet.enchere.bo.Article;
+import fr.eni.projet.enchere.bo.User;
 
 /**
  * Servlet implementation class ModifyArticle
@@ -14,8 +20,15 @@ import javax.servlet.http.HttpServletResponse;
 public class ModifyArticle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VUE = "/WEB-INF/JSP/ModifyArticle.jsp";
-       
-
+	
+	private static final String ARTICLE = "article"; 
+	private static final String NAME_ARTICLE = "nameArticle";   
+	private static final String DESCRIPTION = "description";
+	private static final String DATE_START_AUCTION = "dateStartAuction";
+	private static final String DATE_END_AUCTION = "EndAuction";
+	private static final String PRICE_START = "points";
+	
+	private static final String USER = "user";
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -30,7 +43,34 @@ public class ModifyArticle extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		// Commme le noArticle n'est pas généré dans le formulaire, j'essaye de le récupérer en tant qu'attribut lier à la requête
+		// AUCUNE IDEE DE CI CA FONCTIONNE
+		Article article = (Article) request.getAttribute(ARTICLE);
+		int noArticle = article.getNoArticle();
+		
+		String nameArticle = request.getParameter(NAME_ARTICLE);
+		String description = request.getParameter(DESCRIPTION);
+		String dateStartAuction = request.getParameter(DATE_START_AUCTION);
+		String dateEndAuction = request.getParameter(DATE_END_AUCTION);
+		int priceStart = Integer.valueOf(request.getParameter(PRICE_START));
+		Integer priceSold = null; // On peut modifier l'article puisqu'il n'est pas vendu
+		
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute(USER);
+		int noUser = user.getNo_utilisateur();
+		
+		//TODO La catégorie
+		
+//		private int noArticle;
+//		private String nameArticle;
+//		private String description;
+//		private LocalDateTime dateStartAuction;
+//		private LocalDateTime dateEndAuction;
+//		private int priceStart;
+//		private int priceSold;
+//		private int noUser;
+//		private int noCategorie;
 	}
 
 }
